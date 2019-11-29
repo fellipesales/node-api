e1ae03b9646a6d6a3e77a151864720584848d6a2
Fellipe Sales <fsales@rcasistemas.com.br>
2019-11-29 17:32:27 -0300

    product controller store method

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const requireDir = require("require-dir");
 
 //Iniciando o App
 const app = express();
+app.use(express.json());
 
 //Iniciando o DB
 mongoose.connect("mongodb+srv://admin:123@cluster0-xh1fw.azure.mongodb.net/nodeapi", {
